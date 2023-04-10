ef160bc5c55713c0520fdb6e20813d17ff8b3e52
Juan Gabriel Maioli <juanmaioli@gmail.com>
2023-04-10 08:44:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import  * as dotenv from 'dotenv'
+
+dotenv.config()
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+console.log(process.env.DB_NAME)
+// console.log(process.env)
